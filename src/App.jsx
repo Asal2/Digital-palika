0364ee8d239eb9988ce8106c95692ae25096d3e1
Ashal Sharma <ashalsharma68@gmail.com>
2023-06-08 11:43:08 +0545

    styling change

--- src/App.jsx
+++ src/App.jsx
@@ -20,30 +20,60 @@ function App() {
   console.log(formData);
 
   return (
-    <div className="mt-56 ">
-      <div className="ml-96 w-96 h-24 bg-blue-950 text-white">
-        <div className="pt-5"><h1 className="text-2xl">Welcome to</h1><h6>Digital Palika</h6></div>
+    <div className="mt-56 h-70">
+      <div className="ml-96 w-96 h-24 bg-blue-950 text-white rounded-t-lg">
+        <div className="pt-5">
+          <h1 className="text-2xl">Welcome to</h1>
+          <h6>Digital Palika</h6>
+        </div>
       </div>
-      <form className="ml-96 w-96 h-60 bg-white border border-gray-300 p-4" >
-       <label className="mr-72 text-green-500 bottom-1.5">Email</label>
-        <br/>
+      <form className="ml-96 w-96 h-64  bg-white border border-gray-300 rounded-b-lg">
+        <br />
         <input
-          className= "mr-96z mb-2 w-80 h-10 border border-gray-500"
+          className="mr-96z mb-2 w-80 h-10 border border-gray-500 rounded-t-lg rounded-b-lg"
           type="email"
           name="email"
           value={formData.email}
           onChange={handleChange}
         />
-        <label className="mr-64  text-green-500">Password</label>
+        <div style={{ position: "relative" }}>
+          <div
+            style={{
+              position: "absolute",
+              top: "-3.7rem",
+              left: "3.5rem",
+              fontSize: "15px",
+              fontWeight: 500,
+              color: "#41b883",
+            }}
+          >
+            <label className="mr-72 text-green-500 bg-white">Email</label>
+          </div>
+        </div>
         <br/>
         <input
-        className=" mr-96z mb-2 w-80 h-10 border border-gray-500"
+          className=" mr-96z mb-2 w-80 h-10 border border-gray-500 rounded-t-lg rounded-b-lg"
           type="password"
           name="password"
           value={formData.password}
           onChange={handleChange}
         />
-        <br/>
+
+        <div style={{ position: "relative" }}>
+          <div
+            style={{
+              position: "absolute",
+              top: "-3.7rem",
+              left: "3.5rem",
+              fontSize: "15px",
+              fontWeight: 500,
+              color: "#41b883",
+            }}
+          >
+            <label className="mr-72 text-green-500 bg-white">Password</label>
+          </div>
+        </div>
+        <br />
         <input
           className="mr-1"
           id="okayToRemember"
@@ -54,8 +84,10 @@ function App() {
         />
         <label className="mr-24">Remember me</label>
         <label>Forgot Password?</label>
-        <br/>
-        <button className="mt-3 bg-green-500 w-80 h-9">Login</button>
+        <br />
+        <button className="mt-3 bg-green-500 w-80 h-9 rounded-t-lg rounded-b-lg">
+          Login
+        </button>
       </form>
     </div>
   );
